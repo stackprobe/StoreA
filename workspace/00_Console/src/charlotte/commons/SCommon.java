ed8602fe50800fde37a65694119252911df13884
stackprobe <stackprobes@gmail.com>
2023-02-06 20:29:55 +0900

    Voyager 1 is 23784583716.532 kilometers away from the Sun.

--- workspace/00_Console/src/charlotte/commons/SCommon.java
+++ workspace/00_Console/src/charlotte/commons/SCommon.java
@@ -1,32 +1,109 @@
 package charlotte.commons;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 共通機能・便利機能はできるだけこのクラスに集約する。
  * @author mt
  *
  */
 public class SCommon {
-	private static class S_AnonyAutoCloseable implements AutoCloseable {
-		private Runnable _routine;
-		
-		public S_AnonyAutoCloseable(Runnable routine) {
-			_routine = routine;
+
+	// TODO
+	// TODO
+	// TODO
+
+	// PRIMITIVE[] -> List<PRIMITIVE> ここから
+
+	public static List<Boolean> toList(boolean[] arr) {
+		List<Boolean> list = new ArrayList<Boolean>(arr.length);
+
+		for (int index = 0; index < arr.length; index++) {
+			list.add(arr[index]);
 		}
-		
-		@Override
-		public void close() throws Exception {
-			if (_routine != null) {
-				_routine.run();
-				_routine = null;
-			}
+		return list;
+	}
+
+	public static List<Byte> toList(byte[] arr) {
+		List<Byte> list = new ArrayList<Byte>(arr.length);
+
+		for (int index = 0; index < arr.length; index++) {
+			list.add(arr[index]);
 		}
+		return list;
 	}
-	
-	public static AutoCloseable getAnonyAutoCloseable(Runnable routine) {
-		return new S_AnonyAutoCloseable(routine);
+
+	public static List<Character> toList(char[] arr) {
+		List<Character> list = new ArrayList<Character>(arr.length);
+
+		for (int index = 0; index < arr.length; index++) {
+			list.add(arr[index]);
+		}
+		return list;
 	}
-	
+
+	public static List<Short> toList(short[] arr) {
+		List<Short> list = new ArrayList<Short>(arr.length);
+
+		for (int index = 0; index < arr.length; index++) {
+			list.add(arr[index]);
+		}
+		return list;
+	}
+
+	public static List<Integer> toList(int[] arr) {
+		List<Integer> list = new ArrayList<Integer>(arr.length);
+
+		for (int index = 0; index < arr.length; index++) {
+			list.add(arr[index]);
+		}
+		return list;
+	}
+
+	public static List<Long> toList(long[] arr) {
+		List<Long> list = new ArrayList<Long>(arr.length);
+
+		for (int index = 0; index < arr.length; index++) {
+			list.add(arr[index]);
+		}
+		return list;
+	}
+
+	public static List<Float> toList(float[] arr) {
+		List<Float> list = new ArrayList<Float>(arr.length);
+
+		for (int index = 0; index < arr.length; index++) {
+			list.add(arr[index]);
+		}
+		return list;
+	}
+
+	public static List<Double> toList(double[] arr) {
+		List<Double> list = new ArrayList<Double>(arr.length);
+
+		for (int index = 0; index < arr.length; index++) {
+			list.add(arr[index]);
+		}
+		return list;
+	}
+
+	// PRIMITIVE[] -> List<PRIMITIVE> ここまで
+
+	// List<PRIMITIVE> -> PRIMITIVE[] ここから
+
+	public static boolean[] toPrimitiveArray(List<Boolean> list) {
+		boolean[] arr = new boolean[list.size()];
+
+		for (int index = 0; index < list.size(); index++) {
+			arr[index] = list.get(index);
+		}
+		return arr;
+	}
+
 	// TODO
 	// TODO
 	// TODO
+
+	// List<PRIMITIVE> -> PRIMITIVE[] ここまで
 }
