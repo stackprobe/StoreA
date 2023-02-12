0845ad28f987d812c238ced01fa5707de474f71e
stackprobe <stackprobes@gmail.com>
2023-02-12 23:50:27 +0900

    Voyager 1 is 23793543204.816 kilometers away from the Sun.

--- Dev/Program/Test20230111/Claes20200001/Claes20200001/Tests/Test0004.cs
+++ Dev/Program/Test20230111/Claes20200001/Claes20200001/Tests/Test0004.cs
@@ -11,9 +11,54 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
+			string[] table = new string[]
+			{
+				"１２３",
+				"４５６",
+				"７８９",
+			};
 
+			Func<int, int, char> g = (x, y) => table[y][x];
 
+			Action p = () =>
+			{
+				for (int y = 0; y < 3; y++)
+				{
+					for (int x = 0; x < 3; x++)
+						Console.Write(g(x, y));
 
+					Console.WriteLine();
+				}
+				Console.WriteLine("--");
+			};
+
+			p();
+			g = Rot90(g);
+			p();
+			g = Rot90(g);
+			p();
+			g = Rot90(g);
+			p();
+			g = Rot90(g);
+			p();
+		}
+
+		private Func<int, int, char> Rot90(Func<int, int, char> g)
+		{
+			g = Twist(g);
+			g = Mirror(g);
+
+			return g;
+		}
+
+		private Func<int, int, char> Twist(Func<int, int, char> g)
+		{
+			return (x, y) => g(y, x);
+		}
+
+		private Func<int, int, char> Mirror(Func<int, int, char> g)
+		{
+			return (x, y) => g(2 - x, y);
 		}
 	}
 }
